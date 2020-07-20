9bba550c42e9015d665f0ea1ac0bdc9037c3d6a5
ethriel3695 <ethriel3695@gmail.com>
2020-07-19 23:59:14 -0600

    Added blogs and cta to mdx posts

---
+++ content/post/ctaData.js
@@ -0,0 +1,12 @@
+export const ctaData = {
+  section: {
+    title: 'Finally a tool to build Production Web Applications Easily',
+    text: {
+      text:
+        'Subscribe to follow our progress including repos, quick-start quides, tutorials and more! This tool is to ease your life as a developer if it does not then please Unsubscribe at any time or reach out and let us know what we can do!',
+    },
+    buttonText: 'Yes Please!',
+    externalLink: 'https://sendfox.com/form/104zrv/10r8en',
+    slug: 'start-creating',
+  },
+};
